aa7ad895b1f185c7af20dd8ffa65ca26e054d211
robin0P <muhammedhadiyildiz@gmail.com>
2022-01-30 10:52:27 +0300

    I just finished 12H Python Full Course

--- exam.py
+++ exam.py
@@ -1740,5 +1740,257 @@ window.mainloop()
 
 # window.mainloop()
 # ------------------------------------------------------------
-# text editor
+# tic-tac-toe game
 # ------------------------------------------------------------
+# from tkinter import *
+# import random
+
+# def next_turn(row, column):
+#     global player
+#     if buttons[row][column]['text'] == "" and check_winner() is False:
+#         if player == players[0]:
+#             buttons[row][column]['text'] = player
+#             if check_winner() is False:
+#                 player = players[1]
+#                 label.config(text = (players[1] + " turn"))
+#             elif check_winner() is True:
+#                 label.config(text = (players[0] + " wins"))
+#             elif check_winner() == "Tie":
+#                 label.config(text = "Tie!")
+#         else:
+#             buttons[row][column]['text'] = player
+#             if check_winner() is False:
+#                 player = players[0]
+#                 label.config(text = (players[0] + " turn"))
+#             elif check_winner() is True:
+#                 label.config(text = (players[1] + " wins"))
+#             elif check_winner() == "Tie":
+#                 label.config(text = "Tie!")
+
+# def check_winner():
+
+#     for row in range(3):
+#         if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
+#             buttons[row][0].config(bg = "Green")
+#             buttons[row][1].config(bg = "Green")
+#             buttons[row][2].config(bg = "Green")
+#             return True
+
+#     for column in range(3):
+#         if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+#             buttons[0][column].config(bg = "Green")
+#             buttons[1][column].config(bg = "Green")
+#             buttons[2][column].config(bg = "Green")
+#             return True
+
+#     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons [2][2]['text'] != "":
+#         buttons[0][0].config(bg = "Green")
+#         buttons[1][0].config(bg = "Green")
+#         buttons[2][2].config(bg = "Green")
+#         return True
+
+#     elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != "":
+#         buttons[0][2].config(bg = "Green")
+#         buttons[1][1].config(bg = "Green")
+#         buttons[2][0].config(bg = "Green")
+#         return True
+
+#     elif empty_spaces() is False:
+#         return True
+
+#     else:
+#         return False
+
+# def empty_spaces():
+#     spaces = 9
+
+#     for row in range(3):
+#         for column in range(3):
+#             if buttons[row][column]['text'] != "":
+#                 spaces -= 1
+
+#     if spaces == 0:
+#         return False
+
+# def new_game():
+#     global player
+
+#     player = random.choice(players)
+#     label.config(text = player + " turn")
+#     for row in range(3):
+#         for column in range(3):
+#             buttons[row][column].config(text = "", bg = "#F0F0F0")
+
+# window = Tk()
+# window.title("Tic-Tac-Toe")
+
+# players = ["x","o"]
+# player = random.choice(players)
+# buttons = [[0,0,0],
+#            [0,0,0],
+#            [0,0,0]]
+
+# label = Label(text = player + " turn", font = ("Fira Mono", 40))
+# label.pack(side = "top")
+
+# reset_button = Button(text = "Restart", font = ("Fira Mono", 20), command = new_game)
+# reset_button.pack(side = "top")
+
+# frame = Frame(window)
+# frame.pack()
+
+# for row in range(3):
+#     for column in range(3):
+#         buttons[row][column] = Button(frame, text = "", font = ("Fira Mono", 40), width = 5, height = 2,
+#                                       command = lambda row = row, column = column: next_turn(row, column))
+#         buttons[row][column].grid(row = row, column = column)
+
+# window.mainloop()
+# ----------------------------------------------------------
+# snake game
+# ----------------------------------------------------------
+# from tkinter import *
+# import random
+
+# GAME_WIDTH = 700
+# GAME_HEIGHT = 700
+# SPEED = 50
+# SPACE_SIZE = 50
+# BODY_PARTS = 3
+# SNAKE_COLOR = "#00FF00"
+# FOOD_COLOR = "#FF0000"
+# BACKGROUND_COLOR = "#000000"
+
+# class Snake:
+#     def __init__(self):
+#         self.body_size = BODY_PARTS
+#         self.coordinates = []
+#         self.squares = []
+
+#         for i in range(0, BODY_PARTS):
+#             self.coordinates.append([0,0])
+
+#         for x, y in self.coordinates:
+#             square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR, tag = "snake")
+#             self.squares.append(square)
+
+# class Food:
+#     def __init__(self):
+#         x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+#         y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE)-1) * SPACE_SIZE
+
+#         self.coordinates = [x,y]
+
+#         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR, tag = "food")
+
+# def next_turn(snake, food):
+#     x, y = snake.coordinates[0]
+
+#     if direction == "up":
+#         y -= SPACE_SIZE
+
+#     elif direction == "down":
+#         y += SPACE_SIZE
+
+#     elif direction == "left":
+#         x -= SPACE_SIZE
+
+#     elif direction == "right":
+#         x += SPACE_SIZE
+
+#     snake.coordinates.insert(0, (x, y))
+#     square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = SNAKE_COLOR)
+#     snake.squares.insert(0, square)
+
+#     if x == food.coordinates[0] and y == food.coordinates[1]:
+#         global score
+#         score += 1
+#         label.config(text = "Score:{}".format(score))
+#         canvas.delete("food")
+#         food = Food()
+
+#     else:
+#         del snake.coordinates[-1]
+#         canvas.delete(snake.squares[-1])
+#         del snake.squares[-1]
+
+#     if check_collisions(snake):
+#         game_over()
+#     else:
+#         window.after(SPEED, next_turn, snake, food)
+
+# def change_direction(new_direction):
+#     global direction
+
+#     if new_direction == 'left':
+#         if direction != 'right':
+#             direction = new_direction
+
+#     elif new_direction == 'right':
+#         if direction != 'left':
+#             direction = new_direction
+
+#     elif new_direction == 'up':
+#         if direction != 'down':
+#             direction = new_direction
+
+#     elif new_direction == 'down':
+#         if direction != 'up':
+#             direction = new_direction
+
+
+# def check_collisions(snake):
+#     x, y = snake.coordinates[0]
+#     if x < 0 or x >= GAME_WIDTH:
+#         return True
+
+#     elif y < 0 or y >= GAME_HEIGHT:
+#         return True
+
+#     for body_part in snake.coordinates[1:]:
+#         if x == body_part[0] and y == body_part[1]:
+#             print("GAME OVER")
+#             return True
+
+#     return False
+
+# def game_over():
+#     canvas.delete(ALL)
+#     canvas.create_text(canvas.winfo_width() / 2, canvas.winfo_height() / 2,
+#                        font = ("Fira Mono", 70), text = "GAME OVER", fill = "red", tag = "gameover")
+
+# window = Tk()
+# window.title("Snake game")
+# window.resizable(False, False)
+
+# score = 0
+# direction = 'down'
+# label = Label(window, text = "Score:{}".format(score), font = ("Fira Mono", 40))
+# label.pack()
+
+# canvas = Canvas(window, bg = BACKGROUND_COLOR, height = GAME_HEIGHT, width = GAME_WIDTH)
+# canvas.pack()
+
+# window.update()
+
+# window_width = window.winfo_width()
+# window_height = window.winfo_height()
+# screen_width = window.winfo_screenwidth()
+# screen_height = window.winfo_screenheight()
+
+# x = int((screen_width/2) - (window_width/2))
+# y = int((screen_height/2) - (window_height/2))
+
+# window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+# window.bind("<Left>", lambda event: change_direction('left'))
+# window.bind("<Right>", lambda event: change_direction('right'))
+# window.bind("<Up>", lambda event: change_direction('up'))
+# window.bind("<Down>", lambda event: change_direction('down'))
+
+# snake = Snake()
+# food = Food()
+
+# next_turn(snake, food)
+
+# window.mainloop()
